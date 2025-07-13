436bd83d77cb89a16c6b8c978dfaeaf406997572
Ibrahim Khalid <ibrahim.k@sybertechnology.com>
2025-07-13 20:55:12 +0400

    Fixed user spec

--- src/main/java/com/karrardelivery/controller/spec/UserSpec.java
+++ src/main/java/com/karrardelivery/controller/spec/UserSpec.java
@@ -8,14 +8,14 @@ import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
 
 @And({
-    @Spec(path = "firstName", spec = LikeIgnoreCase.class),
-    @Spec(path = "middleName", spec = LikeIgnoreCase.class),
-    @Spec(path = "lastName", spec = LikeIgnoreCase.class),
-    @Spec(path = "phone", spec = LikeIgnoreCase.class),
-    @Spec(path = "email", spec = LikeIgnoreCase.class),
-    @Spec(path = "role", spec = Equal.class),
-    @Spec(path = "enabled", spec = Equal.class),
-    @Spec(path = "deleted", spec = Equal.class, constVal = "false")
+    @Spec(params = "firstName", path = "firstName", spec = LikeIgnoreCase.class),
+    @Spec(params = "middleName", path = "middleName", spec = LikeIgnoreCase.class),
+    @Spec(params = "lastName", path = "lastName", spec = LikeIgnoreCase.class),
+    @Spec(params = "phone", path = "phone", spec = LikeIgnoreCase.class),
+    @Spec(params = "email", path = "email", spec = LikeIgnoreCase.class),
+    @Spec(params = "query", path = "role", spec = Equal.class),
+    @Spec(params = "role", path = "enabled", spec = Equal.class),
+    @Spec(params = "deleted", path = "deleted", spec = Equal.class, constVal = "false")
 
 })
 public interface UserSpec extends Specification<User> {
